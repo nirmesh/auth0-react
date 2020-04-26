507e4ce278e0b248fe46ece2668c32f5fd2863d0
nirmesh <you@example.com>
2020-04-26 13:30:04 +0530

    added router

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Router } from "react-router-dom";
 import PropTypes from "prop-types";
 import AuthContext from "./AuthContext";
 
